9fb55a3d4205ee2227f73acf0b85d96e0e34336d
bruce aldridge <bruce@incode.co.nz>
2016-03-15 10:35:13 +1300

    Add prefixes for AU

--- src/en_AU/PhoneNumberValidator.php
+++ src/en_AU/PhoneNumberValidator.php
@@ -6,4 +6,13 @@ namespace webtoolsnz\validators\en_AU;
 class PhoneNumberValidator extends \webtoolsnz\validators\PhoneNumberValidator
 {
     public $expectedRegion = 'AU';
+    public $available_prefixes = [
+        '02',
+        '03',
+        '04',
+        '05', // Unused; formerly Location Independent Communication Services, to be assigned as a second mobile code in 2017
+              // https://en.wikipedia.org/wiki/Telephone_numbers_in_Australia#cite_ref-05-numbers_8-0
+        '07',
+        '08',
+    ];
 }
